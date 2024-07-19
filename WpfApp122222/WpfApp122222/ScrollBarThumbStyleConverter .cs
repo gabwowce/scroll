d277a327bb9af779071de37eaef0ccbc0a85a813
Gabrielė <134537965+gabwowce@users.noreply.github.com>
2024-07-19 12:47:16 +0300

    Add files via upload

---
+++ WpfApp122222/WpfApp122222/ScrollBarThumbStyleConverter .cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WpfApp122222
+{
+    public class ScrollBarThumbStyleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var tag = value as string;
+            if (!string.IsNullOrEmpty(tag))
+            {
+                return Application.Current.Resources["VerticalScrollBarImg"];
+            }
+            return Application.Current.Resources["VerticalScrollBar"];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
